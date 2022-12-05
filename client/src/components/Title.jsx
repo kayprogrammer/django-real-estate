28c0efc49e3fc97374609b555cbb2e608a320060
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-05 14:22:29 +0100

    feat: add custom Title component with Helmet

---
+++ client/src/components/Title.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Title = ({title, description, keywords}) => {
+  return (
+    <Helmet>
+        <Title>{title}</Title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+
+    </Helmet>
+  )
+}
+
+Title.defaultProps={
+    title: "Welcome to Real Estate",
+    description: "We sell the best properties in town",
+    keywords: "land, real estate, best value",
+};
+
+export default Title;
--- client/src/pages/PropertiesPage.jsx
+++ client/src/pages/PropertiesPage.jsx
@@ -5,6 +5,7 @@ import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
 import Property from '../components/Property';
 import { getProperties, reset } from '../features/properties/propertySlice';
+import Title from '../components/Title';
 
 
 const PropertiesPage = () => {
@@ -21,25 +22,28 @@ const PropertiesPage = () => {
 
   if (isLoading) return <Spinner />;
   return (
-    <Container>
-        <Row>
-            <Col className='mg-top text-center'>
-                <h1>Our Catalog of properties</h1>
-                <hr className='hr-text'/>
-            </Col>
-        </Row>
-        {
-          <>
-            <Row className='mt-3'>
-              {properties.map((property) => (
-                <Col key={property.id} sm={12} md={6} lg={4} xl={3}>
-                  <Property property={property}/>
-                </Col>
-              ))}
-            </Row>
-          </>
-        }
-    </Container>
+    <>
+      <Title title="Our Properties Catalog"/>
+      <Container>
+          <Row>
+              <Col className='mg-top text-center'>
+                  <h1>Our Catalog of properties</h1>
+                  <hr className='hr-text'/>
+              </Col>
+          </Row>
+          {
+            <>
+              <Row className='mt-3'>
+                {properties.map((property) => (
+                  <Col key={property.id} sm={12} md={6} lg={4} xl={3}>
+                    <Property property={property}/>
+                  </Col>
+                ))}
+              </Row>
+            </>
+          }
+      </Container>
+    </>
   );
 };
 
