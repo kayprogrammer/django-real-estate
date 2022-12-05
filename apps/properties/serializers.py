3d97608daba897adf10cc944be6f8fde6f408cf8
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-05 04:38:54 +0100

    feat: add property slice

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -7,6 +7,13 @@ from .models import Property, PropertyViews
 class PropertySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     country = CountryField(name_only=True)
+    profile_photo = serializers.SerializerMethodField()
+    cover_photo = serializers.SerializerMethodField()
+    photo1 = serializers.SerializerMethodField()
+    photo2 = serializers.SerializerMethodField()
+    photo3 = serializers.SerializerMethodField()
+    photo4 = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Property
@@ -30,6 +37,7 @@ class PropertySerializer(serializers.ModelSerializer):
             "bedrooms",
             "bathrooms",
             "advert_type",
+            'profile_photo',
             "cover_photo",
             "photo1",
             "photo2",
@@ -42,6 +50,23 @@ class PropertySerializer(serializers.ModelSerializer):
     def get_user(self, obj):
         return obj.user.username
 
+    def get_profile_photo(self, obj):
+        return obj.user.profile.profile_photo.url
+
+    def get_cover_photo(self, obj):
+        return obj.cover_photo.url
+
+    def get_photo1(self, obj):
+        return obj.photo1.url
+
+    def get_photo2(self, obj):
+        return obj.photo2.url
+
+    def get_photo3(self, obj):
+        return obj.photo3.url
+    
+    def get_photo4(self, obj):
+        return obj.photo4.url
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
     country = CountryField(name_only=True)
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import propertyReducer from '../features/properties/propertySlice';
 export const store = configureStore({
   reducer: {
+    properties: propertyReducer,
   },
 });
