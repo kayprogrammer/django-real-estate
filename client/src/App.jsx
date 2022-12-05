fe7c7f320cae447958ba8a450d86fc98b8ac36ec
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-05 03:59:42 +0100

    chore: setup basic home and properties page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import PropertiesPage from './pages/PropertiesPage';
 
 const App = () => {
   return (
-    <div className="App">
-     <h1>Welcome to the real estate</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/properties" element={<PropertiesPage />}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
